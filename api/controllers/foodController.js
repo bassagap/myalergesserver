faf408796ac8f16f96eef0c0155eb205c046abe2
Paula Bassagañas Òdena <paula.odena@gmail.com>
2017-08-13 17:52:32 +0200

    Response returned changed

--- api/controllers/foodController.js
+++ api/controllers/foodController.js
@@ -4,13 +4,9 @@ var mongoose = require('mongoose'),
   Food = mongoose.model('Food');
   var mongodb = require('mongodb');
   var axios      = require('axios');
-
-
 exports.get_all_ingredients = function(req, res) {
   var barcode = req.query.barcode;
-  var food_information;
-  var food = new Food();      // create a new instance of the Bear model
-  console.log("barcode: ", barcode);
+  var food = new Food();
   axios.get("https://world.openfoodfacts.org/api/v0/product/" + barcode + ".json")
    .then(function (response) {
      food.name = response.data.product.product_name;
@@ -23,12 +19,10 @@ exports.get_all_ingredients = function(req, res) {
          }
        }
      }
-     console.log("food name: " + food.name);
      food.ingredients = response.data.product.ingredients;
      food.save(function(err) {
-
      });
-   res.json(response.data);
+   res.json(food);
    })
    .catch(function (error) {
      res.send(error);
