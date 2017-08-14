d83280f2a1dc62b90628743528c7daaa5ba25d34
Paula Bassagañas Òdena <paula.odena@gmail.com>
2017-08-14 20:20:56 +0200

    API users authentication working bug fixing 6

--- api/persistence/passport.js
+++ api/persistence/passport.js
@@ -2,11 +2,12 @@ var JwtStrategy = require('passport-jwt').Strategy;
 
 // load up the user model
 var User = require('../models/userModel');
-var config = require('../persistence/userDAO'); // get db config file
+//var config = require('./userDAO'); // get db config file
+
 
 module.exports = function(passport) {
   var opts = {};
-  opts.secretOrKey = config.secret;
+  opts.secretOrKey = 'devdacticIsAwesome';  // config.secret;
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     User.findOne({id: jwt_payload.id}, function(err, user) {
           if (err) {
